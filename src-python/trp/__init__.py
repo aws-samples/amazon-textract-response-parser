27a44cdb6138c993a25d7b75901d558f4ea57235
Martin Schade <schadem@amazon.com>
2021-07-12 12:04:59 -0700

    Bump version: 0.1.13 → 0.1.14

--- src-python/trp/__init__.py
+++ src-python/trp/__init__.py
@@ -7,7 +7,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 logger = logging.getLogger(__name__)
 
-__version__ = '0.1.13'
+__version__ = '0.1.14'
 
 class BaseBlock():
     def __init__(self, block, blockMap):
