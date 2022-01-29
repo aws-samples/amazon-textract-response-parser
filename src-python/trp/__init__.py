64c9c23d9228717730e527df820b30e4c9a4acb6
Martin Schade <schadem@amazon.com>
2022-01-28 18:59:59 -0800

    Bump version: 0.1.25 → 0.1.26

--- src-python/trp/__init__.py
+++ src-python/trp/__init__.py
@@ -8,7 +8,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 logger = logging.getLogger(__name__)
 
-__version__ = '0.1.25'
+__version__ = '0.1.26'
 
 ENTITY_TYPE_COLUMN_HEADER = "COLUMN_HEADER"
 
