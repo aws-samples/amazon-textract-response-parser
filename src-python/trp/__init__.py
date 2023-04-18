160c30ee24af6f753b493fe13e8a6b295c2ba5f8
schadem <45048633+schadem@users.noreply.github.com>
2023-04-18 10:38:34 -0700

    Bump version: 0.1.44 → 0.1.45

--- src-python/trp/__init__.py
+++ src-python/trp/__init__.py
@@ -8,7 +8,7 @@ logging.getLogger(__name__).addHandler(NullHandler())
 
 logger = logging.getLogger(__name__)
 
-__version__ = '0.1.44'
+__version__ = '0.1.45'
 
 ENTITY_TYPE_COLUMN_HEADER = "COLUMN_HEADER"
 ENTITY_TYPE_MERGED_CELL = "MERGED_CELL"
