5be8b6d6451dbb060dabde2c4df6c551f172c3be
Alex Thewsey <thewsey@amazon.com>
2024-01-17 16:33:37 +0800

    doc(js): Minor API model commentary additions

--- src-js/src/api-models/expense.ts
+++ src-js/src/api-models/expense.ts
@@ -2,6 +2,7 @@
  * Expense analysis Textract API models used by the Textract response parser.
  *
  * This file collects types/interfaces specific to Textract's expense/invoice/receipt analysis APIs
+ * as per https://docs.aws.amazon.com/textract/latest/dg/invoices-receipts.html
  */
 
 // Local Dependencies:
