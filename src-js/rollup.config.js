711aab2373468d349735630500343dfa82e98dfa
Alex Thewsey <thewsey@amazon.com>
2024-01-17 15:40:29 +0800

    chore(js): Upgrade terser and other DevDeps
    Switch from deprecated rollup-plugin-terser to @rollup/plugin-terser

--- src-js/rollup.config.js
+++ src-js/rollup.config.js
@@ -1,7 +1,7 @@
 // External Dependencies:
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default [
   {
